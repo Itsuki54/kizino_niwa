b644a5df7f5164fcd919ce14be44078b70afeced
Itsuki54 <5shi.icyu@gmail.com>
2024-05-31 13:29:22 +0900

    a

---
+++ src/components/template/HomeLayout.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Footer } from "../organisms/layout/Footer";
+
+interface HomeLayoutProps {
+  header: React.ReactNode;
+  rightBar: React.ReactNode;
+  leftBar: React.ReactNode;
+  main: React.ReactNode;
+}
+
+export function HomeLayout({
+  header,
+  rightBar,
+  leftBar,
+  main,
+}: HomeLayoutProps) {
+  return (
+    <div className="flex-col h-screen ">
+      <div className="  flex-col top-0 sticky ">
+        {header}
+        <div className="flex h-full ">
+          <div className=" bg-gray-50 w-1/5">{leftBar}</div>
+          <main className=" flex-col w-full bg-white mb-14 ">{main}</main>
+          <nav className="  h-full bg-gray-50 w-1/5">
+            <div className="w-full  mx-auto ">{rightBar}</div>
+          </nav>
+        </div>
+      </div>
+      <div className="bottom-0">
+        <Footer />
+      </div>
+    </div>
+  );
+}
